30c8237f44a1728795e3e472180e0816e3b26b35
Feyi <anjuwontobi99@gamil.com>
2022-09-10 12:42:20 +0100

    Got started

---
+++ react-course/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./index.css";
+import Header from "./header";
+import Footer from "./footer";
+import Content from "./content";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-course/src/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Hello Everyone</h1>
+    </header>
+  );
+};
+
+export default Header;
